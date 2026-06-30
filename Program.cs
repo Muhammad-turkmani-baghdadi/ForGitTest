cd8b6ad8a7d97325889ac775b1dd5c95db971b6a
Muhammad-turkmani-baghdadi <mohammadturkmanibaghdadi@gmail.com>
2026-06-30 11:02:54 +0300

    Update from the github

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace ForGitTests
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("hello this update for git");
+            Console.WriteLine("this update is form github.");
+            
         }
     }
 }

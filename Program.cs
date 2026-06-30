5615bd98f48d70b122addc5a2194253bdf87b943
Muhammad-turkmani-baghdadi <mohammadturkmanibaghdadi@gmail.com>
2026-06-30 11:01:14 +0300

    add Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace ForGitTests
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello this update for git");
         }
     }
 }
